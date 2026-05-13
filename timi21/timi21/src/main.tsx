f6239a3f5f7ef78560f83f5eb3e778afbc614aaf
as24-nemandi-ntv-is <as24@nemandi.ntv.is>
2026-05-14 03:03:40 +0800

    add layout

--- timi21/timi21/src/main.tsx
+++ timi21/timi21/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import FramerMotion from './pages/framer-motion.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<App />}>
+    </Route>
+    <Route path='/framer-motion' element={<FramerMotion></FramerMotion>}></Route>
+  </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
---
+++ timi21/timi21/src/pages/framer-motion.tsx
@@ -0,0 +1,5 @@
+export function framerMotion() {
+    return <div>Framer motion síða </div>
+}
+
+export default framerMotion;
