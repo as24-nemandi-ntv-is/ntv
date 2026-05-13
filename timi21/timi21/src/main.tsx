a51f7a6f04451b17ff3bfb4b4258525e6146bb63
as24-nemandi-ntv-is <as24@nemandi.ntv.is>
2026-05-14 05:03:39 +0800

    update

--- timi21/timi21/src/main.tsx
+++ timi21/timi21/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import FramerMotion from './pages/framer-motion.tsx'
+import CanvasConfettiPage from './components/animations/CanvasConfetti.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
